8db0f3a2303f4e39c2179f60bcee51739bdcd11f
Julien Poulton <julien.poulton@gmail.com>
2026-03-04 11:37:26 +0100

    I BROKE EVERYTHING with generators. I'll try and remove them and see

--- worlds/fakutori/__init__.py
+++ worlds/fakutori/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 import os
 import collections
 import json
@@ -18,9 +19,10 @@ from worlds.generic import Rules
 from worlds.stardew_valley.stardew_rule import state
 from .items import FakutoriItem
 from .locations import FakutoriLocation
-from .options import FakutoriOptions
+from .options import FakutoriOptions, VictoryCondition
 from .data.data import colors
 from worlds.generic.Rules import set_rule, add_rule, forbid_item, add_item_rule
+import inspect
 
 def data_path(*args):
     return os.path.join(os.path.dirname(__file__), 'data', *args)
@@ -32,6 +34,27 @@ def print_return(func):
         return result
     return wrapper
 
+@dataclass
+class Ingredient:
+    ingredientType: str
+    blockName: Optional[str] = None
+    property: Optional[str] = None
+    color: Optional[str] = None
+    quantity: Optional[int] = None
+@dataclass
+class Recipe:
+    type: str
+    product: str
+    ingredients: List[Ingredient]
+    byproduct: Optional[str] = None
+@dataclass
+class BlockData:
+    name: str
+    id: int
+    category: str
+    color: str
+    properties: List[str]
+    unlockedByDefault: bool
 
 class Fakutori(World):
     """Fakutori is a laid-back and colorful automation game;
@@ -51,52 +74,61 @@ class Fakutori(World):
     options: FakutoriOptions  # typing hints for option results
     topology_present = True  # show path to required location checks in spoiler
 
-    blocks = []
+    blocks: List[BlockData] = []
     with open(data_path('blocks.json'), 'r') as stream:
         blocks_json = json.load(stream)
-    # TODO: add generators, because why not
-    blocks = [b for b in blocks_json['blocks'] if 'Generator' not in b['name']]    
+    blocks = [BlockData(**b) for b in blocks_json['blocks']]
 
-    recipes = []
+    recipes: List[Recipe] = []
     with open(data_path('recipes.json'), 'r') as stream:
         recipes_json = json.load(stream)
-    recipes = [r for r in recipes_json['recipes'] if r['type'] != 'Generator']
+    recipes = [
+        Recipe(
+            type=r["type"],
+            product=r["product"],
+            byproduct=r["byproduct"],
+            ingredients=[Ingredient(**ing) for ing in r["ingredients"]]
+        )
+        for r in recipes_json["recipes"]
+    ]
 
     item_name_to_id = {}
     location_name_to_id = {}
     for unlockable in blocks:
-        item_name_to_id[unlockable['name']] = unlockable['id']
-        if not unlockable['unlockedByDefault']:
-            location_name_to_id[unlockable['name']] = unlockable['id']
-    item_name_to_id["500 gold"] = 1000  
-    item_name_to_id["500 mana"] = 1001  
-    item_name_to_id["Full starpower"] = 1002
+        item_name_to_id[unlockable.name] = unlockable.id
+        if not unlockable.unlockedByDefault and not unlockable.name == 'Disassembler':
+            location_name_to_id[unlockable.name] = unlockable.id
+    item_name_to_id["500 gold"] = 1000
+    item_name_to_id["1000 gold"] = 1001
+    item_name_to_id["500 mana"] = 1002
+    item_name_to_id["Full starpower"] = 1003
 
     # Items can be grouped using their names to allow easy checking if any item
     # from that group has been collected. Group names can also be used for !hint
     item_name_groups = {}
     for item in blocks:
-        if item['category'] != 'Machine':
-            color = item['color']
+        if item.category != 'Machine':
+            color = item.color
             if not color in item_name_groups:
                 item_name_groups[color] = []
-            item_name_groups[color].append(item['name'])
+            item_name_groups[color].append(item.name)
         
-        for property in item['properties']:
+        for property in item.properties:
             if not property in item_name_groups:
                 item_name_groups[property] = []
-            item_name_groups[property].append(item['name'])
+            item_name_groups[property].append(item.name)
 
     def classify_item(self, item: str) -> ItemClassification:
         for u in self.blocks:
-            if u['name'] == item:
-                if u['category'] == 'Machine':
+            if u.name == item:
+                if u.category == 'Machine':
+                    if 'Generator' in u.name:
+                        return ItemClassification.progression 
                     return ItemClassification.useful
                 return ItemClassification.progression
         return ItemClassification.filler
 
     def create_item(self, item: str) -> FakutoriItem:
-        # this is called when AP wants to create an item by name (for plando, start inventory, item links) or when you call it from your own code
         classification = self.classify_item(item)
         return FakutoriItem(item, classification, self.item_name_to_id[item], self.player)
 
@@ -106,32 +138,37 @@ class Fakutori(World):
         return FakutoriItem(event, ItemClassification.progression, None, self.player)
 
     def create_items(self) -> None:
-        # Add items to the Multiworld.
-        # If there are two of the same item, the item has to be twice in the pool.
-        # Which items are added to the pool may depend on player options, e.g. custom win condition like triforce hunt.
-        # Having an item in the start inventory won't remove it from the pool.
-        # If you want to do that, use start_inventory_from_pool
-
         for b in self.blocks:
-            if not b['unlockedByDefault']:
-                self.multiworld.itempool.append(self.create_item(b['name']))
-
-        # itempool and number of locations should match up.
-        # If this is not the case we want to fill the itempool with junk.
+            if b.name == 'Disassembler':
+                continue
+            if not b.unlockedByDefault:
+                self.multiworld.itempool.append(self.create_item(b.name))
         
+        # Add base elements
         for b in self.blocks:
-            if b['unlockedByDefault']:
-                self.multiworld.push_precollected(self.create_item(b['name']))
-                
+            if b.unlockedByDefault and b.category == 'Raw element':
+                self.multiworld.push_precollected(self.create_item(b.name))
+        
+        # Add base mahchines
+        for b in self.blocks:
+            if b.unlockedByDefault and b.category == 'Machine':
+                if self.options.start_with_base_machines.value:
+                    self.multiworld.push_precollected(self.create_item(b.name))
+                else:
+                    self.multiworld.itempool.append(self.create_item(b.name))
+            
+        if self.options.start_with_disassembler.value:
+            self.multiworld.push_precollected(self.create_item("Disassembler"))
+        else:
+            self.multiworld.itempool.append(self.create_item("Disassembler"))
+
         total_locations = len(self.multiworld.get_unfilled_locations(self.player))
-        itempool += [self.create_filler() for _ in range(total_locations - len(itempool))]
-      
+        self.multiworld.itempool += [self.create_filler() for _ in range(total_locations - len(self.multiworld.itempool))]
+  
     filler_choices = ("Full starpower", "500 mana", "500 gold", "1000 gold")
     filler_weights = (1, 2, 1, 4)
 
     def get_filler_item_name(self) -> str:
-        if self.options.stabilize_item_pool:
-            return "Nothing"
         return self.random.choices(self.filler_choices, self.filler_weights)[0]
 
 
@@ -141,14 +178,14 @@ class Fakutori(World):
         pass
 
     def create_regions(self) -> None:
-        # Add regions to the multiworld. One of them must use the origin_region_name as its name ("Menu" by default).
-        # Arguments to Region() are name, player, multiworld, and optionally hint_text
         menu_region = Region("Menu", self.player, self.multiworld)
-        self.multiworld.regions.append(menu_region)  # or use += [menu_region...]
+        self.multiworld.regions.append(menu_region)  
 
         main_region = Region("Factory", self.player, self.multiworld)
-        # add main area's locations to main area (all but final boss)
     
+        if not self.options.start_with_disassembler.value:
+            self.location_id_to_name[5] = 'Disassembler'
+        
         locations = {}
         for location_name in self.location_name_to_id.keys():
             locations[location_name] = self.location_name_to_id[location_name]
@@ -158,10 +195,11 @@ class Fakutori(World):
 
         for i in range(self.options.extra_shop_checks.value):
             main_region.add_locations({f"Extra Shop {i+1}": 1000 + i}, FakutoriLocation)
+            self.location_name_to_id[f"Extra Shop {i+1}"] = 1000 + i
         
         self.multiworld.regions.append(main_region)
 
-        menu_region.connect(main_region) 
+        menu_region.connect(main_region)
 
     def can_craft_block(self, state: CollectionState, blockName: str) -> bool:
         every_craftable_block = self.get_every_craftable_block(state)
@@ -172,54 +210,54 @@ class Fakutori(World):
         unlocked_blocks = state.prog_items[self.player].keys()
         return self.get_every_craftable_block_from(unlocked_blocks)
     
-    def get_every_craftable_block_from(self, unlocked_blocks: List[str]) -> List[str]:
+    def get_every_craftable_block_from(self, unlocked_blocks: List[str], assume_generators = False) -> List[str]:
         virtual_collection = set()
 
         unlocked_recipes = []
         for name in unlocked_blocks:
-            unlocked_recipes += [r for r in self.recipes if r['product'] == name]
+            unlocked_recipes += [r for r in self.recipes if r.product == name]
         for b in self.blocks:
-            if b['category'] == 'Raw element' and b['name'] in unlocked_blocks:
-                virtual_collection.add(b['name'])
+            if b.category == 'Raw element' and b.name in unlocked_blocks and ('Generator ' + b.name.lower() in unlocked_blocks or assume_generators):
+                virtual_collection.add(b.name)
         
         new_crafted = True
         while new_crafted:
             new_crafted = False
             for recipe in unlocked_recipes:
-                if recipe["product"] not in virtual_collection and self.can_do_recipe(virtual_collection, recipe):
-                    virtual_collection.add(recipe['product'])
+                if recipe.product not in virtual_collection and self.can_do_recipe(virtual_collection, recipe):
+                    virtual_collection.add(recipe.product)
                     new_crafted = True
                     break
         return virtual_collection
 
-    def has_ingredient(self, collection: Set[str], ingredient) -> bool:
-        blockName = ingredient['blockName']
-        quantity = ingredient['quantity']
-        ingredientType = ingredient['ingredientType']
+    def has_ingredient(self, collection: Set[str], ingredient: Ingredient) -> bool:
+        blockName = ingredient.blockName
+        quantity = ingredient.quantity
+        ingredientType = ingredient.ingredientType
 
         if ingredientType == 'Block':
             return blockName in collection
         elif ingredientType == 'Property':
             prop_counter = self.count_properties(collection)
-            return prop_counter[ingredient['property']] >= quantity
+            return prop_counter[ingredient.property] >= quantity
         elif ingredientType == 'Color':
             color_counter = self.count_colors(collection)
-            return color_counter[ingredient['color']] >= quantity
+            return color_counter[ingredient.color] >= quantity
         return True
 
     def count_properties(self, collection: Set[str]) -> Dict[str, int]:
         Counter = collections.Counter()
         for block_name in collection:
-            block = next(b for b in self.blocks if b['name'] == block_name)
-            for property in block['properties']:
+            block = next(b for b in self.blocks if b.name == block_name)
+            for property in block.properties:
                 Counter[property] += 1
         return Counter
 
     def count_colors(self, collection: Set[str]) -> Dict[str, int]:
         Counter = collections.Counter()
         for block_name in collection:
-            block = next(b for b in self.blocks if b['name'] == block_name)
-            color = block['color']
+            block = next(b for b in self.blocks if b.name == block_name)
+            color = block.color
             if color == 'Colorless':
                 for c in colors:
                     Counter[c] += 1
@@ -231,39 +269,57 @@ class Fakutori(World):
         color_counter = self.count_colors(collection)
         return len(color_counter) >= 7
 
-    def can_do_recipe(self, collection: Set[str], recipe) -> bool:
-        if recipe['type'] == 'Starstruck':
-            return "Shooting star" in collection and all(self.has_ingredient(collection, ingredient) for ingredient in recipe['ingredients'])
-        elif recipe['type'] == 'Quasar':
+    def can_do_recipe(self, collection: Set[str], recipe: Recipe) -> bool:
+        if recipe.type == 'Generator':
+            return recipe.product in collection
+        if recipe.type == 'Starstruck':
+            return "Shooting star" in collection and all(self.has_ingredient(collection, ingredient) for ingredient in recipe.ingredients)
+        elif recipe.type == 'Quasar':
             return "Black hole" in collection
-        elif recipe['type'] == 'Void':
+        elif recipe.type == 'Void':
             return "Antimatter" in collection
-        elif recipe['type'] == 'EvolvingFire':
-            return "Wood" in collection or "Oil" in collection and "Fire" in collection
-        elif recipe['product'] == 'Rainbow':
+        elif recipe.type == 'EvolvingFire':
+            return ("Wood" in collection or "Oil" in collection) and "Fire" in collection
+        elif recipe.product == 'Rainbow':
             return self.can_rainbow(collection)
-        elif recipe['type'] in ('Combine', 'Combust', 'Quickening', 'BlackHole', 'Time', 'DissolveMetals', 'Fall'):
-            return all(self.has_ingredient(collection, ingredient) for ingredient in recipe['ingredients'])
+        elif recipe.type in ('Combine', 'Combust', 'Quickening', 'BlackHole', 'Time', 'DissolveMetals', 'Fall'):
+            return all(self.has_ingredient(collection, ingredient) for ingredient in recipe.ingredients)
         return True
 
     def set_rules(self) -> None:
         already_has_rule = set()
         for recipe in self.recipes:
-            if recipe['product'] in already_has_rule:
-                add_rule(
-                    self.multiworld.get_location(recipe['product'], self.player),
-                    lambda state, r=recipe: self.can_craft_block(state, r['product']),
-                    'or'
-                )
-            else:
-                set_rule(
-                    self.multiworld.get_location(recipe['product'], self.player),
-                    lambda state, r=recipe: self.can_craft_block(state, r['product'])
-                )
-                already_has_rule.add(recipe['product'])
-        
-        progression_items = [item['name'] for item in self.blocks if self.classify_item(item['category']) == ItemClassification.progression]
-        self.multiworld.completion_condition[self.player] = lambda state: state.has_all(progression_items, self.player)
+            # Raw Elements do not result in a check
+            if recipe.product in ('Water', 'Air', 'Fire', 'Earth'):
+                continue
+            products = [recipe.product]
+            if recipe.byproduct:
+                products.append(recipe.byproduct)
+
+            for p in products:
+                if p in already_has_rule:
+                    add_rule(
+                        self.multiworld.get_location(p, self.player),
+                        lambda state, r=recipe: self.can_craft_block(state, r.product),
+                        'or'
+                    )
+                else:
+                    set_rule(
+                        self.multiworld.get_location(p, self.player),
+                        lambda state, r=recipe: self.can_craft_block(state, r.product)
+                    )
+                    already_has_rule.add(recipe.product)
+
     
+        if self.options.victory_condition.value == VictoryCondition.option_all_blocks_discovered:
+            progression_items = [item.name for item in self.blocks if self.classify_item(item.category) == ItemClassification.progression]
+            progression_items_that_are_locations = [item for item in progression_items if item in self.location_name_to_id]
+            self.multiworld.completion_condition[self.player] = lambda state,pi=progression_items_that_are_locations: all([loc.name in pi for loc in state.locations_checked])
+        elif self.options.victory_condition.value == VictoryCondition.option_spawn_quasar:
+            self.multiworld.completion_condition[self.player] = lambda state: any([loc.name == "Quasar" for loc in state.locations_checked])
+        else:
+            # TODO: challenges
+            pass
+
     def fill_slot_data(self) -> Dict[str, Any]:
         return self.options.as_dict("victory_condition", "shop_price", "extra_shop_checks")
--- worlds/fakutori/test/__init__.py
+++ worlds/fakutori/test/__init__.py
@@ -1,26 +1,49 @@
 from __future__ import annotations
+from argparse import Namespace
 from typing import Any, Dict, List, Optional, Tuple
+from BaseClasses import CollectionState
+from test import multiworld
 from test.bases import WorldTestBase
+from worlds import AutoWorld
+from worlds.apquest.game import player
+from worlds.fakutori.options import VictoryCondition
+from .. import Fakutori
 
 
 class MyGameTestBase(WorldTestBase):
     game = "Fakutori"
 
+    def test_debug(self) -> None:
+        state = CollectionState(self.multiworld)
+        print('base items:')
+        for item in state.prog_items[self.player]:
+            print(f'  {item}')
+        self.collect_all_but([], state)
+        print('all items:')
+        for item in state.prog_items[self.player]:
+            print(f'  {item}')
+
+        print('all locations:')
+        for location in self.multiworld.get_locations(self.player):
+            print(f'  {location.name}')
+        
+        assert False
+
     def test_fire(self) -> None:
         unlocks = ['Fire', 'Earth', 'Water', 'Wood', 'Yellow fire', 'Blue fire', 'Air']
-        all_craftable = self.world.get_every_craftable_block_from(unlocks)
+        all_craftable = self.world.get_every_craftable_block_from(unlocks, assume_generators=True)
         print(all_craftable)
         assert all_craftable == {'Fire', 'Wood', 'Earth', 'Air', 'Blue fire', 'Water', 'Yellow fire'}
     
     def test_no_wood(self) -> None:
         unlocks = ['Fire', 'Earth', 'Wood', 'Yellow fire', 'Blue fire']
-        all_craftable = self.world.get_every_craftable_block_from(unlocks)
+        all_craftable = self.world.get_every_craftable_block_from(unlocks, assume_generators=True)
         print(all_craftable)
         assert all_craftable == {'Fire', 'Earth'}
     
     def test_time(self) -> None:
         unlocks = ['Time', 'Fire', 'Water', 'Smoke', 'Steam', 'Ether']
-        all_craftable = self.world.get_every_craftable_block_from(unlocks)
+        all_craftable = self.world.get_every_craftable_block_from(unlocks, assume_generators=True)
         print(all_craftable)
         assert all_craftable == {'Time', 'Fire', 'Water', 'Steam', 'Ether'}
     
@@ -34,8 +57,14 @@ class MyGameTestBase(WorldTestBase):
             'Shooting star', # yellow
             'Rainbow'
         ]
-        all_craftable_no_pink = self.world.get_every_craftable_block_from(unlocks_no_pink)
+        all_craftable_no_pink = self.world.get_every_craftable_block_from(unlocks_no_pink, assume_generators=True)
         assert "Rainbow" not in all_craftable_no_pink
+
+    def test_water(self) -> None:
+        unlocks = ['Water']
+        all_craftable = self.world.get_every_craftable_block_from(unlocks)
+        print(all_craftable)
+        assert all_craftable == set()
 
     def test_lava(self) -> None:
         locations = ["Lava"]
@@ -68,6 +97,6 @@ class MyGameTestBase(WorldTestBase):
             'Ether', # pink
             'Rainbow'
         ]
-        all_craftable_with_pink = self.world.get_every_craftable_block_from(unlocks_with_pink)
+        all_craftable_with_pink = self.world.get_every_craftable_block_from(unlocks_with_pink, assume_generators=True)
         print(all_craftable_with_pink)
         assert "Rainbow" in all_craftable_with_pink
